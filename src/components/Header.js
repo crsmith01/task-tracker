3bcc3e3204c99ff591a1868f80f9083d3451b7dc
crsmith01 <catsmith24601@gmail.com>
2021-09-10 11:09:42 -0400

    created components file, added Header component, updated index.css

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+
+
+const Header = ({title}) => {
+    return (
+        <div>
+            <h1>{title}</h1>
+        </div>
+    )
+}
+
+Header.defaultProps = {
+    title: "Tast Tracker"
+}
+
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired
+}
+
+export default Header;
