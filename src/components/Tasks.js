d2ca3e232003830b15f5963f030155ec1ba9312d
crsmith01 <catsmith24601@gmail.com>
2021-09-15 10:44:55 -0400

    installed react-icons, updated what task passes

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Task from './Task';
 
 const Tasks = ({tasks}) => {
     
     return (
         <div>
             {tasks.map((task) => (
-                <h3 key={task.id}>{task.text}</h3>))}
+                <Task key={task.id} task={task}/>
+            ))}
         </div>
     )
 }
