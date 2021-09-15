a0c476680885adebfbc126f7c8059e19b69df0a7
crsmith01 <catsmith24601@gmail.com>
2021-09-15 12:57:56 -0400

    filtering through tasks, added onDelete function

--- src/App.js
+++ src/App.js
@@ -7,28 +7,42 @@ import { useState } from 'react';
 function App() {
   const [tasks, setTasks] = useState([
     {
-        id: 1,
-        text: 'Doctor appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
+      id: 1,
+      text: 'Doctor appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true,
     },
     {
-        id: 2,
-        text: 'Meeting with coding classmates',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true,
+      id: 2,
+      text: 'Meeting with coding classmates',
+      day: 'Feb 6th at 1:30pm',
+      reminder: true,
     },
     {
-        id: 3,
-        text: 'Grocery shopping',
-        day: 'Feb 5th at 12:30pm',
-        reminder: false,
+      id: 3,
+      text: 'Grocery shopping',
+      day: 'Feb 5th at 12:30pm',
+      reminder: false,
     }
-])
+  ])
+
+  // Delete Task
+  const deleteTask = (id) => {
+    console.log('delete', id);
+    // filter is a high order array method that takes in a function. 
+    // Don't want to show task with id because we're deleting it
+    // setting tasks to the filtered tasks = immutable state (cannot change state itself)
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+
   return (
     <div className="container">
-     <Header/>
-     <Tasks tasks={tasks} />
+      <Header />
+      <Tasks
+        tasks={tasks}
+        onDelete={deleteTask}
+      />
     </div>
   );
 }
