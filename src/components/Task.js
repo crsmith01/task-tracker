69e5e41259bd9fb81d4cd0adb65f4c7c26a87a88
crsmith01 <catsmith24601@gmail.com>
2021-09-21 10:55:02 -0400

    added contitional class to reminder

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,9 +3,11 @@ import { FaTimes } from 'react-icons/fa';
 
 const Task = ({ task, onDelete, onToggle }) => {
     return (
+        // task class with condition in template literal - if the task.reminder is true then class reminder. else there is no class on it
+        <div className={`task ${task.reminder ? 'reminder' : ''}`} 
         // need the id so call a function and then pass in onToggle
         // needs to be task.id not just id = since passing in task as a prop (line 4)
-        <div className='task' onDoubleClick={() => onToggle(task.id)}>
+        onDoubleClick={() => onToggle(task.id)}>
             <h3>
                 {task.text}
                 <FaTimes
